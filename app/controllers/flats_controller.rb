9fd26a6865cbaad2e5389d1449ddf74227be242d
Emanuel Borges <ebbmango@tuta.io>
2022-10-20 20:41:13 -0300

    index page done

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,39 @@
+class FlatsController < ApplicationController
+  before_action :set_flat, only: %i[show edit update destroy]
+
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+    @flat.destroy
+    redirect_to flats_path, status: :see_other
+  end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+RNG = Random.new
+AMOUNT = 10
+
+puts "Generating #{AMOUNT} flats..."
+
+AMOUNT.times do
+  flat = Flat.new(
+    name: "#{%w[apartment flat studio].sample.capitalize} at #{Faker::Address.city}",
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.paragraph,
+    price_per_night: (RNG.rand(3.0) * 100).to_i,
+    number_of_guests: rand(1..5)
+  )
+  flat.save!
+
+  puts "#{flat.name} with id #{flat.id} successfully generated"
+end
+
+puts 'Done!'
